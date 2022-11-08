9b12dc804ecc9f63442fb11e3c86f876a096b615
Loïc Faure-Lacroix <lamerstar@gmail.com>
2022-11-07 19:01:50 -0500

    Use entrypoints to load commands and extend existing groups

---
+++ odoo_tools/cli/click/gen.py
@@ -0,0 +1,10 @@
+import click
+import logging
+
+
+_logger = logging.getLogger(__name__)
+
+
+@click.group()
+def gen():
+    pass
---
+++ odoo_tools/cli/registry.py
@@ -0,0 +1,37 @@
+from pkg_resources import iter_entry_points
+
+
+class CommandRegistry(object):
+    def __init__(self):
+        self.groups = {}
+        self.main_command = None
+        self.loaded = set()
+
+    def set_main(self, command):
+        self.main_command = command
+
+    def get(self, name):
+        return self.groups[name]
+
+    def register(self, name, command):
+        self.groups[name] = command
+
+    def register_commands(self):
+        for ep in iter_entry_points(group='odootools.command'):
+            registry.register(ep.name, ep.load())
+
+        for ep in iter_entry_points(group='odootools.command.ext'):
+            command = self.groups[ep.name]
+            obj = ep.load()
+            command.add_command(obj)
+
+    def load(self):
+        for key, value in self.groups.items():
+            if key in self.loaded:
+                continue
+            self.loaded.add(key)
+            self.main_command.add_command(value)
+
+
+registry = CommandRegistry()
+registry.register_commands()
